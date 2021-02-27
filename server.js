570395193923c8a3b36bf4a18fa3b0648837cff5
klin4994 <kevlin129@hotmail.com>
2021-02-27 13:58:22 +1100

    Adding root inquirer questions

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ connection.connect((err) => {
 
 });
 let staffList = [];
-
+// Obtain all staff for manager array
 const getManager = () => {
     connection.query ('Select id, first_name, last_name FROM employees', (err, res) => {
         if (err) throw err;
@@ -33,8 +33,9 @@ const getManager = () => {
         })
     })
 }
-
-const addPerson = () => {getManager()
+// // Task List: Add a staff
+const addStaff = () => {
+    getManager()
     roleList = ['Sales Lead', 'Salesperson', 'Lead Engineer',
     'Software Engineer', 'Accountant', 'Legal Team Lead',
     'Lawyer']    
@@ -61,56 +62,82 @@ const addPerson = () => {getManager()
         type: 'list'
     }
 ]).then((result) => {
-
-    let roleIndex = 0;
-    for(i=0; i<roleList.length; i++) {
-        if (roleList[i] === result.role ) {
-            roleIndex = i+1;
-        }
-        console.log(roleList[i])
-        console.log(result.role)
-        console.log(i)
-        console.log('-----------------------------')
-    }
-    let managerIndex = 0;
-    if (staffList.length === 1) {
-        managerIndex = null;
-    } 
-    else {
-        for(i=0; i<staffList.length; i++) {
-            if (staffList[i] === result.manager ) {
-                managerIndex = i;
+    // Get role id for the corresponding role selected
+        let roleIndex = 0;
+        for(i=0; i<roleList.length; i++) {
+            if (roleList[i] === result.role ) {
+                roleIndex = i+1;
             }
+            // Get staff's id for the corresponding manager selected
+            let managerIndex = 0;
+            // If no staff names available to choose, return null
+            if (staffList.length === 1) {
+                managerIndex = null;
+            } else {
+                for(i=0; i<staffList.length; i++) {
+                    if (staffList[i] === result.manager ) {
+                        managerIndex = i;
+                    }
+                }
+            }
+            // Query to add a new row with the new staff's details
+            let query = 'INSERT INTO employees SET ?';
+            console.log(result);
+            console.log(roleIndex);
+            connection.query(query,
+                {
+                first_name: result.first_name,
+                last_name: result.last_name,
+                role_id: roleIndex,
+                manager_id: managerIndex,
+                },
+                (err, res) => {
+                    if (err) throw err;    
+                }
+            ),
+            root()
         }
-    }
-    let query = 'INSERT INTO employees SET ?';
-    console.log(result);
-    console.log(roleIndex);
-    connection.query(query,
-        {
-        first_name: result.first_name,
-        last_name: result.last_name,
-        role_id: roleIndex,
-        manager_id: managerIndex,
-    },
-        (err, res) => {
-            if (err) throw err;
-            
-        }
-    )
-    
-
-        // connection.query('INSERT INTO employees SET ? WHERE ?',
-        //     {
-        //         role_id:
-        //     })
-        connection.end();
-
-    
-})
+        
+    })
 }
 
+const taskList = ['Exit the application', 'Add a staff', 'View all employees', 'Update employee roles']
 
-addPerson();
+
+// sample inquirer
+const root = () => {
+    inquirer.prompt ([
+        {
+        name: 'task',
+        message: 'What would you like to do:',
+        choices: taskList,
+        type: 'list'
+        }
+    ]).then ((response) => {
+        console.log(response.task)
+        switch (response.task) {
+            case 'Exit the application':
+                connection.end()
+                break;
+            case 'Add a staff':
+                addStaff()
+                break;
+            case 'View all employees':
+                viewEmployees()
+                break;
+            case 'Update employee roles':
+                updateRole()
+                break;
+        }
+    })
+}
+//Root inquirer
+init = () => {
+    root();
+    
+}   
+init();
+
+
 
 
